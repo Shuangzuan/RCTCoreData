88c8e33ac88d8f839dcf804433cfd576cd5df006
Shuangzuan <shuangzuan.he@icloud.com>
2015-06-04 22:59:50 +0800

    Add app state

--- RCTCoreData.ios.js
+++ RCTCoreData.ios.js
@@ -4,14 +4,29 @@
  */
 'use strict';
 
-var NativeRCTCoreData = require('NativeModules').RCTCoreData;
+var React = require('react-native');
+var {
+  AppStateIOS
+} = React;
+var NativeRCTCoreData = require('NativeModules').CoreData;
 var invariant = require('invariant');
 
 /**
  * High-level docs for the RCTCoreData iOS API can be written here.
  */
 
+var CHANGE = 'change';
+
 var RCTCoreData = {
+  load: function() {
+    AppStateIOS.addEventListener(CHANGE, this._handleAppStateChange);
+  },
+  unload: function() {
+    AppStateIOS.removeEventListener(CHANGE, this._handleAppStateChange);
+  },
+  _handleAppStateChange: function(currentAppState) {
+    console.log(currentAppState);
+  },
   test: function() {
     NativeRCTCoreData.test();
   }
